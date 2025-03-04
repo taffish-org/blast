73e2aa1ad9c761e458176df619d08496d0eab8ea
Kaiyuan Han <hermit_han@163.com>
2025-03-04 15:42:39 +0800

    add :functions to blast.info.lisp

--- blast.info.lisp
+++ blast.info.lisp
@@ -3,4 +3,37 @@
  (:author         "KaiyuanHan")
  (:latest-version "2.16.0" "2025-03-03")
  (:dependencies)
- (:versions       "2.16.0"))
+ (:versions       "2.16.0")
+ (:functions      "blast_formatter"
+                  "blast_formatter_vdb"
+                  "blast_vdb_cmd"
+                  "blastdb_aliastool"
+                  "blastdb_path"
+                  "blastdbcheck"
+                  "blastdbcmd"
+                  "blastn"
+                  "blastn.REAL"
+                  "blastn_vdb"
+                  "blastp"
+                  "blastp.REAL"
+                  "blastx"
+                  "blastx.REAL"
+                  "cleanup-blastdb-volumes.py"
+                  "convert2blastmask"
+                  "deltablast"
+                  "dustmasker"
+                  "legacy_blast.pl"
+                  "makeblastdb"
+                  "makembindex"
+                  "makeprofiledb"
+                  "psiblast"
+                  "rpsblast"
+                  "rpstblastn"
+                  "segmasker"
+                  "tblastn"
+                  "tblastn.REAL"
+                  "tblastn_vdb"
+                  "tblastx"
+                  "tblastx.REAL"
+                  "update_blastdb.pl"
+                  "windowmasker"))
